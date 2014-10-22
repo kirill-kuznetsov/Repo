59a83a20f50e370ea5446f099bb606fa725f0565
Kirill Kuznetsov <kirill.kuznets@gmail.com>
2014-10-22 17:12:29 +0400

    Basic matching works

--- CV/src/Tester.java
+++ CV/src/Tester.java
@@ -1,21 +1,116 @@
 import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_core.*;
 import org.bytedeco.javacpp.opencv_features2d.*;
+import org.bytedeco.javacpp.opencv_nonfree;
 import org.bytedeco.javacpp.opencv_nonfree.*;
 
-import static org.bytedeco.javacpp.opencv_highgui.imread;
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Comparator;
+
+import static org.bytedeco.javacpp.opencv_features2d.drawKeypoints;
+import static org.bytedeco.javacpp.opencv_features2d.drawMatches;
+import static org.bytedeco.javacpp.opencv_highgui.*;
 
 /**
  * Created by kkuznets on 10.09.2014.
  */
 public class Tester {
-    public static void main(String[] testImages){
-        SIFT sift = new SIFT();
-        final opencv_core.Mat image = imread(testImages[0]);
-        final KeyPoint keyPoints = new KeyPoint();
-        // JVM crashes when JavaCV native binaries and OpenCV binaries are build with different versions of VisualStudio
-        // For instance, JavaCV is build with VC10 and OpenCV with VC11.
 
-        sift.detect(image, keyPoints);
-        float size = keyPoints.size();
+    public static final String SIFT = "SIFT";
+
+    public static void main(String[] testImages){
+//        SIFT sift = new SIFT(10);
+
+        ORB sift = new ORB();
+        int patternsNumber = testImages.length;
+
+        final Mat image1 = imread(testImages[0]);
+        final Mat image2 = imread(testImages[1]);
+
+        final KeyPoint keyPoints1 = new KeyPoint();
+        final KeyPoint keyPoints2 = new KeyPoint();
+
+        Mat featureDescriptors1 = new Mat();
+        Mat featureDescriptors2 = new Mat();
+
+        detectKeypoints(sift, image1, keyPoints1, featureDescriptors1);
+        detectKeypoints(sift, image2, keyPoints2, featureDescriptors2);
+
+        BFMatcher matcher = new BFMatcher();
+        DMatch matches = new DMatch(10);
+        matcher.match(featureDescriptors1, featureDescriptors2, matches);
+
+        Mat outImage = new Mat();
+        Scalar keypointColor = new Scalar(CvScalar.WHITE);
+
+        matches = selectBest(matches, 10);
+        drawMatches(image1, keyPoints1, image2, keyPoints2, matches,  outImage);
+
+        File image = new File(testImages[0]);
+        String resultPath = image.getAbsolutePath();
+        imwrite(resultPath + "resultImage.jpg", outImage);
+        namedWindow("Result");
+        imshow("Result", outImage);
+        while(waitKey() != 'q');
+
+        return;
     }
+
+    private static void detectKeypoints(ORB sift, Mat image, KeyPoint keyPoints, Mat featureDescriptors) {
+//        sift.detectAndCompute(image, new Mat(), keyPoints, featureDescriptors);
+        new SIFT().apply(image, new Mat(), keyPoints, featureDescriptors);
+    }
+
+    private static DMatch selectBest(DMatch matches, int size) {
+        int oldPosition = matches.position();
+        DMatch a[] = new DMatch[matches.capacity()];
+        for (int i = 0; i < a.length; i++) {
+            DMatch src = matches.position(i);
+            DMatch dest = new DMatch();
+            copy(src, dest);
+            a[i] = dest;
+        }
+        // Reset position explicitly to avoid issues from other uses of this
+        // position-based container.
+        matches.position(oldPosition);
+
+        // Sort
+
+        DMatch aSorted[] = a;
+        java.util.Arrays.sort(aSorted, new DistanceComparator());
+
+        // DMatch aSorted[]=sort(a);
+
+        // Create new JavaCV list
+        DMatch best = new DMatch(size);
+        for (int i = 0; i < size; i++) {
+            // Since there is no may to `put` objects into a list DMatch,
+            // We have to reassign all values individually, and hope that API
+            // will not any new ones.
+            copy(aSorted[i], best.position(i));
+        }
+
+        // Set position to 0 explicitly to avoid issues from other uses of this
+        // position-based container.
+        best.position(0);
+
+        return best;
+    }
+
+    private static void copy(DMatch src, DMatch dest) {
+        // TODO: use Pointer.copy() after JavaCV/JavaCPP 0.3 is released
+        // (http://code.google.com/p/javacpp/source/detail?r=51f4daa13d618c6bd6a5556ff2096d0e834638cc)
+        // dest.put(src)
+        dest.distance(src.distance());
+        dest.imgIdx(src.imgIdx());
+        dest.queryIdx(src.queryIdx());
+        dest.trainIdx(src.trainIdx());
+    }
+
+    static class DistanceComparator implements Comparator<DMatch> {
+        public int compare(DMatch o1, DMatch o2) {
+            return (int) Math.signum(o1.distance() - o2.distance());
+        }
+    };
 }
